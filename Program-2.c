81b4360091da2518667e6ad075dbf61aaf337bd8
shabu-m <89658447+shabu-m@users.noreply.github.com>
2021-09-20 20:20:40 +0530

    language C

---
+++ Program-2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	printf("input a = %d ",n);
+	printf("\n");
+  	for(j=1,i=1;j<=n;i=i+2,j++) 	
+  			         
+  	  printf(" %d ",i); 
+}
