94de7e0dfd6504421c7723986a0c2c54ab1e706c
shabu-m <89658447+shabu-m@users.noreply.github.com>
2021-09-20 20:07:43 +0530

    language C

---
+++ Program1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+  char op;
+  double a, b;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &a, &b);
+  switch (op) {
+    case '+'
+      printf("%.1lf + %.1lf = %.1lf", a, b, a + b);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", a, b, a - b);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", a, b, a * b);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", a, b, a / b);
+      break;
+    default:
+      printf("Error! operator is not correct");
+  }
+  return 0;
+}
